8ccf56c30312a747b44d67c2f756fe240753c70a
Simone Fortunato <frtsmn94@gmail.com>
2022-10-17 15:35:14 +0200

    Creazione route API

--- public/js/front.js
+++ public/js/front.js
@@ -12612,7 +12612,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\simon\Desktop\SIMONE\BOOLEAN\lavori\laravel-one-to-many\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\simon\Desktop\SIMONE\BOOLEAN\lavori\laravel-api\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
